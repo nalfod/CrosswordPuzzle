24ccd535efadcd6f5e79cd8c0d965dae6ceb5da8
Mate Michaletzky <mate.michaletzky@evosoft.com>
2023-10-12 09:20:12 +0200

    Tester done

--- GENERATOR/test_copy_methods.py
+++ GENERATOR/test_copy_methods.py
@@ -10,7 +10,7 @@ def modify_matrix(matrix, i):
     
 def rec_cond(matrix):
     # it is enough to return the statement value instead of using if cond: True else: False syntax
-    return matrix[0][1] == 20
+    return matrix[0][1] == 30
 
 def test_copy_method(matrix, copy_method):
     for i in range(3):
@@ -19,34 +19,45 @@ def test_copy_method(matrix, copy_method):
         # usage of f-strings is good for saving space and managing readability
         print(f"Iteration {i}: Before modifying the matrix:")
         print(f"Original matrix:\n{matrix}")
+        print(f"Memory adress of matrix before modifying:\n{id(matrix)}")
+        print(f"Memory adresses of matrix vals before modifying:\n{list(map(id, matrix))}")
+        print()
         print(f"Copy matrix:\n{copy_matrix}")
+        print(f"Memory adress of copy matrix before modifying:\n{id(copy_matrix)}")
+        print(f"Memory adresses of copy matrix vals before modifying:\n{list(map(id, copy_matrix))}")
+        print()
 
         modify_matrix(matrix, i)
     
         print(f"\n\nIteration {i}: After modifying the matrix:")
         print(f"Original matrix:\n{matrix}")
+        print(f"Memory adress of matrix after modifying:\n{id(matrix)}")
+        print(f"Memory adresses of matrix vals after modifying:\n{list(map(id, matrix))}")
+        print()
         print(f"Copy matrix:\n{copy_matrix}")
+        print(f"Memory adress of copy matrix after modifying:\n{id(copy_matrix)}")
+        print(f"Memory adresses of copy matrix vals after modifying:\n{list(map(id, copy_matrix))}")
         
         if rec_cond(matrix):
-            print("0,1 element of the matrix IS 20 ---> return")
+            print(f"\n0,1 element of the matrix IS 30 ---> return")
             return
 
         # match is a replacement of many if else cases. It does the same but easier to read
         match copy_method:
             case 1:
-                print("\nI am using regular assignment")
+                print(f"\n\n***I am using regular assignment for resetting***")
                 matrix = copy_matrix
 
             case 2:
-                print("\nI am using copy.copy")
+                print(f"\n\n***I am using copy.copy for resetting***")
                 matrix = copy.copy(copy_matrix)
 
             case 3:
-                print("\nI am using deepcopy")
+                print(f"\n\n***I am using deepcopy for resetting***")
                 matrix = copy.deepcopy(copy_matrix)
 
             case 4:
-                print("\nI that wierd chatgpt syntax")
+                print(f"\n\n***I do that wierd chatgpt syntax for resetting***")
                 matrix[:] = copy_matrix
 
             case _:
@@ -54,8 +65,12 @@ def test_copy_method(matrix, copy_method):
             
         print(f"\n\nIteration {i}: After setting back the snapshot:")
         print(f"Original matrix:\n{matrix}")
+        print(f"Memory adress of matrix after resetting:\n{id(matrix)}")
+        print(f"Memory adresses of matrix vals after resetting:\n{list(map(id, matrix))}")
         print(f"Copy matrix:\n{copy_matrix}")
-        print("-------------------------------------------")
+        print(f"Memory adress of copy matrix after resetting:\n{id(copy_matrix)}")
+        print(f"Memory adresses of copy matrix vals after resetting:\n{list(map(id, copy_matrix))}")
+        print(f"-------------------------------------------")
 
 init_matrix = [
                 [1, 2],
@@ -65,3 +80,4 @@ init_matrix = [
 test_copy_method(init_matrix, 4)
 
 print(f"\n\nAt the end the init matrix:\n{init_matrix}")
+print(f"Memory adress of matrix after the procedure:\n{id(init_matrix)}")
